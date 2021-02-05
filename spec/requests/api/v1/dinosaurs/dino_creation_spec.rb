6f290ec95cb1285803aa08b195e459358777d0c2
Nick Edwin <nickedwin85@gmail.com>
2021-02-05 11:50:32 -0700

    Tests for dino creation and cage restrictions

--- spec/requests/api/v1/dinosaurs/dino_creation_spec.rb
+++ spec/requests/api/v1/dinosaurs/dino_creation_spec.rb
@@ -6,14 +6,32 @@ describe "API V1 Dinos", type: 'request' do
       capacity: 1
     )
     @cage2 = Cage.create(
-      capacity: 5
+      capacity: 2
+    )
+    @cage3 = Cage.create(
+      capacity: 2
+    )
+    @cage4 = Cage.create(
+      capacity: 2
+    )
+    @dino1 = Dinosaur.create(
+      name: "Leafy Boy",
+      species: "Stegosaurus",
+      food_type: "Herbivore",
+      cage_id: @cage2.id
+    )
+    @dino2 = Dinosaur.create(
+      name: "Big Claw",
+      species: "Velociraptor",
+      food_type: "Carnivore",
+      cage_id: @cage3.id
     )
   end
 
   describe "POST /api/v1/dinosaurs" do
     context "with valid parameters" do
       let(:valid_params) do
-        { name: "Rex",
+        { name: "Little Rex",
           species: "Tyrannosaurus",
           food_type: "Carnivore",
           cage_id: @cage1.id
@@ -24,15 +42,24 @@ describe "API V1 Dinos", type: 'request' do
         post "/api/v1/dinosaurs", params: valid_params
         expect(response).to be_successful
         expect(response.status).to eq(201)
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        cage = Cage.first
+        dino = cage.dinosaurs.last
+
+        expect(json[:data][:id]).to eq(dino.id.to_s)
+        expect(json[:data][:type]).to eq("dinosaur")
+        expect(json[:data][:attributes][:name]).to eq(dino.name)
       end
     end
 
-    context "with valid parameters" do
+    context "with invalid parameters" do
       let(:invalid_params) do
         { name: "",
           species: "Tyrannosaurus",
           food_type: "Carnivore",
-          cage_id: @cage1.id
+          cage_id: @cage4.id
         }
       end
 
@@ -44,6 +71,93 @@ describe "API V1 Dinos", type: 'request' do
         json = JSON.parse(response.body, symbolize_names: true)
 
         expect(json[:data][:errors].first).to eq("Name can't be blank")
+      end
+    end
+
+    context "testing capacity for cages" do
+      let(:valid_params) do
+        { name: "Rex",
+          species: "Tyrannosaurus",
+          food_type: "Carnivore",
+          cage_id: @cage1.id
+        }
+      end
+
+      it "can not add more dinos than cage capacity" do
+        post "/api/v1/dinosaurs", params: valid_params
+        post "/api/v1/dinosaurs", params: valid_params
+
+        expect(response).to_not be_successful
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data][:errors]).to eq("Can not create dinosaur, cage is at capacity")
+      end
+    end
+
+    context "testing mixing species for cages" do
+      let(:valid_params) do
+        { name: "Rex",
+          species: "Tyrannosaurus",
+          food_type: "Carnivore",
+          cage_id: @cage2.id
+        }
+      end
+
+      it "can not mix herbivores and carnivores" do
+        post "/api/v1/dinosaurs", params: valid_params
+
+        expect(response).to_not be_successful
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data][:errors]).to eq("Can not create dinosaur, carvivores can not be mixed!")
+      end
+    end
+
+    context "testing mixing species for cages" do
+      let(:valid_params) do
+        { name: "Rex",
+          species: "Tyrannosaurus",
+          food_type: "Carnivore",
+          cage_id: @cage3.id
+        }
+      end
+
+      it "can not mix carnivores of 2 different species" do
+        post "/api/v1/dinosaurs", params: valid_params
+
+        expect(response).to_not be_successful
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json[:data][:errors]).to eq("Can not create dinosaur, carvivores can not be mixed!")
+      end
+    end
+
+    context "testing same species for cages" do
+      let(:valid_params) do
+        { name: "Big Rex",
+          species: "Tyrannosaurus",
+          food_type: "Carnivore",
+          cage_id: @cage4.id
+        }
+      end
+
+      it "can mix carnivores if they are the same species" do
+        post "/api/v1/dinosaurs", params: valid_params
+        post "/api/v1/dinosaurs", params: valid_params
+
+        expect(response).to be_successful
+
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        cage = Cage.last
+        dino = cage.dinosaurs.last
+
+        expect(json[:data][:id]).to eq(dino.id.to_s)
+        expect(json[:data][:type]).to eq("dinosaur")
+        expect(json[:data][:attributes][:name]).to eq(dino.name)
       end
     end
   end
